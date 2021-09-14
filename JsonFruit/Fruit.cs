15b82f0c7fec5dcb3487b37c1f5cc6f14363aa6f
move <move@zealand.dk>
2021-09-14 12:15:12 +0200

    Add project files.

---
+++ JsonFruit/Fruit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace JsonFruit
+{
+    public class Fruit
+    {
+        [JsonPropertyName("TypeOfFruit")]
+        public string typeOfFruit { get; set; }
+
+        [JsonIgnore]
+        public string secretInfo;
+
+        public Fruit()
+        {
+        }
+
+        public Fruit(string typeOfFruit)
+        {
+            this.typeOfFruit = typeOfFruit;
+        }
+    }
+}
