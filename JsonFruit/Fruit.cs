ffa0556e035405fd5771f45acbe0b7b8e9c49406
move <move@zealand.dk>
2021-09-23 15:24:20 +0200

    removed the json specific tags

--- JsonFruit/Fruit.cs
+++ JsonFruit/Fruit.cs
@@ -1,15 +1,10 @@
 ﻿using System;
-using System.Text.Json.Serialization;
 
 namespace JsonFruit
 {
     public class Fruit
     {
-        [JsonPropertyName("TypeOfFruit")]
         public string typeOfFruit { get; set; }
-
-        [JsonIgnore]
-        public string secretInfo;
 
         public Fruit()
         {
